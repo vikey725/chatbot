2a6c6fdc45e4ac37ede6c81d282d36003ac36ce5
Vikash Kumar <vikash.kumar725@gmail.com>
2024-04-26 12:23:35 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,10 +1,10 @@
 class Configs:
-    BASE_URL = "https://docs.aryaka.com"
-    PAGE_URL = "https://docs.aryaka.com/space/TBW/8192076"
-    PAGE_COMMON_ID = "rw_main"
-    CONTENT_COMMON_ID = "printable_document"
+    BASE_URL = "<base_url_of_the_website>"
+    PAGE_URL = "<Initial_page_url_where you want to start crawling>"
+    PAGE_COMMON_ID = "<Selenium_will_wait_till_this_Id_is_Visible>"
+    CONTENT_COMMON_ID = "<ID_containing_the_content_that_needs_to_be_crawled>"
     MAX_WAITING_TIME = 20
-    DATA_DIR = "page_content"
-    TOP_ELEMENT = "div"
-    PERSIST_DIR = "./storage"
+    DATA_DIR = "page_content" # data will be daved here
+    TOP_ELEMENT = "div" # top element tag
+    PERSIST_DIR = "./storage" 
     IMAGE_DIR = "static"
